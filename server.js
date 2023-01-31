0b3c3e2b3b51747df7323adecf11e14c403120a8
Pratiksha Kurkure <pratikshakurkure850@gmail.com>
2023-01-31 17:07:50 +0530

    scripts added

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ db()
 
 const app = express()
 app.use(express.json())
+app.use(express.static("public"))
 app.use(cors())
 app.use("/blog", require("./routes/blogRoutes"))
 const PORT = process.env.PORT || 5000
